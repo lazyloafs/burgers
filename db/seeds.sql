ab49ef2691bf622e9ac1079b70eb16344f6c5acb
lazyloafs <60455346+lazyloafs@users.noreply.github.com>
2020-06-25 01:37:44 -0400

    :rocket: burgers rock

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, yummy) VALUES ('Onion Burger', true);
+INSERT INTO burgers (name, yummy) VALUES ('Sriracha Burger', true);
+INSERT INTO burgers (name, yummy) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (name) VALUES ('Shrimp Burger');
